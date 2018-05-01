0dd7b386da087219bbf836f6a613f62ebe5c1c6d
ManuelS <manuelsena@me.com>
2018-05-01 15:04:51 -0700

    dbaccess

---
+++ DbAdapter/DbAdapter.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DbAccess.Tools;
+
+namespace DbAccess.DbAdapter
+{
+    public class DbAdapter : IDbAdapter
+    {
+        public IDbConnection Conn { get; private set; }
+        public IDbCommand Cmd { get; private set; }
+        //Dependancy Injection; one will always want to inject to an interface
+        public DbAdapter(IDbCommand command, IDbConnection conn)
+        {
+            Cmd = command;
+            Conn = conn;
+        }
+
+        public List<T> LoadObject<T>(string storedProcedure,
+            IDbDataParameter[] parameters = null) where T : class
+        {
+            List<T> list = new List<T>();
+            //Before
+            //using (SqlConnection conn = new SqlConnection())
+            //using (SqlCommand cmd = new SqlCommand())
+            //after
+            using (IDbConnection conn = Conn)
+            using (IDbCommand cmd = Cmd)
+            {
+                if (conn.State != ConnectionState.Open)
+                    conn.Open();
+                //---------ADO.NET you need these------------
+                cmd.Connection = conn;
+                cmd.CommandTimeout = 5000;
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.CommandText = storedProcedure;
+                // --------------------------------
+                if (parameters != null)
+                {
+                    foreach (IDbDataParameter parameter in parameters)
+                    {
+                        cmd.Parameters.Add(parameter);
+                    }
+
+                }
+
+                IDataReader reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    //TODO: Map to Object
+                    list.Add(DataMapper<T>.Instance.MapToObject(reader));
+                }
+            }
+            return list;
+        }
+
+        public int ExecuteQuery(string storedProcedure,
+            IDbDataParameter[] parameters,
+            Action<IDbDataParameter[]> returnParameters = null)
+        {
+            using (IDbConnection conn = Conn)
+            using (IDbCommand cmd = Cmd)
+            {
+                if (conn.State != ConnectionState.Open)
+                    conn.Open();
+                cmd.Connection = conn;
+                cmd.CommandTimeout = 5000;
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.CommandText = storedProcedure;
+                foreach (IDbDataParameter parameter in parameters)
+                    cmd.Parameters.Add(parameter);
+                int returnValue = cmd.ExecuteNonQuery();
+
+                returnParameters?.Invoke(parameters);
+                //(simplified) 
+                //understanding for student
+                //if (returnParameters != null)
+                //{
+                //    returnParameters(parameters);
+                //}
+                return returnValue;
+            }
+        }
+        /*-------------------------------------------------------*/
+        public T ExecuteDbScalar<T>(string storedProcedure, IDbDataParameter[] parameters)
+        {
+            using (IDbConnection conn = Conn)
+            using (IDbCommand cmd = Cmd)
+            {//ADO.NET
+                if (conn.State != ConnectionState.Open)
+                    conn.Open();
+                cmd.Connection = conn;
+                cmd.CommandTimeout = 5000;
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.CommandText = storedProcedure;
+                foreach (IDbDataParameter parameter in parameters)
+                    cmd.Parameters.Add(parameter);
+
+                object obj = cmd.ExecuteScalar();
+                return (T)obj;
+                //Convert.ChangeType(obj, typeof(T));
+            }
+        }
+    }
+}
---
+++ Tools/DataMapper.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DbAccess.Tools
+{
+    public class DataMapper<T> where T : class
+    {
+        private static readonly DataMapper<T> _instance = new DataMapper<T>();
+        PropertyInfo[] properties;
+
+        public DataMapper()
+        {
+            properties = typeof(T).GetProperties();
+        }
+
+        static DataMapper() { }
+
+        public static DataMapper<T> Instance { get { return _instance; } }
+
+
+        public T MapToObject(IDataReader reader)
+        {
+            IEnumerable<string> columns = reader.GetSchemaTable()
+                .Rows.Cast<DataRow>().Select(
+                //so you dont have to wrry about matching
+                c => c["ColumnName"].ToString().ToLower()).ToList();
+            //createinstance will creat a class of it
+            T obj = Activator.CreateInstance<T>();
+
+            foreach(PropertyInfo pinfo in properties)
+            {
+                if(columns.Contains(pinfo.Name.ToLower()))
+                {
+                    if(reader[pinfo.Name] != DBNull.Value)
+                    {
+                        if(reader[pinfo.Name].GetType() == typeof(decimal))
+                        {
+                            pinfo.SetValue(obj, reader.GetDouble(pinfo.Name));
+                        }
+                        else
+                        {
+                            pinfo.SetValue(obj, 
+                                (reader.GetValue(reader.GetOrdinal(pinfo.Name)) ?? null), 
+                                null);
+                        }
+                    }
+                }
+            }
+            //string test = "test".ChangeString();
+            return obj;
+        }
+    }
+
+    public static class DataHelper
+    {//multiple to look for different 
+        public static double GetDouble(this IDataReader reader, string columnName)
+        {
+            double dbl = 0;
+            double.TryParse(reader[columnName].ToString(), out dbl);
+            return dbl;
+        }
+
+        public static double GetDouble(this IDataReader reader, int columnIndex)
+        {
+            double dbl = 0;
+            double.TryParse(reader[columnIndex].ToString(), out dbl);
+            return dbl;
+        }
+
+    
+    }
+}
